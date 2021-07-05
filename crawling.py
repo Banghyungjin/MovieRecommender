4bdf3a1563fb634e105d507306289658da558d11
HyungJin_Bang <37135305+Banghyungjin@users.noreply.github.com>
2021-07-05 16:33:01 +0900

    Update crawling.py

--- crawling.py
+++ crawling.py
@@ -15,13 +15,32 @@ def get_movie_link(url):
       movie_links_list.append(target_url)
   return movie_links_list
 
-url = "http://movie.naver.com/movie/point/af/list.nhn"
-movie_links_list = get_movie_link(url)
-for movie_link in movie_links_list:
-  res = requests.get(movie_link)
-  soup = BeautifulSoup(res.text, 'html5lib')
-  genres = soup.find_all('#old_content > div.choice_movie_box > div.choice_movie_info > table > tbody > tr:nth-child(1) > td > a:nth-child(1)')
-  print(movie_link, genres)
-  for genre in genres:
-    print(genre)  
+
+def get_genre_list(url):
+  movie_links_list = get_movie_link(url)
+  genre_list = []
+  for movie_url in movie_links_list:
+    print(movie_url)
+    res = requests.get(movie_url)
+    soup = BeautifulSoup(res.text, 'html5lib')
+    genres = soup.find_all('table', class_='info_area')
+    for genre in genres:
+      genre_list.append(genre.a.get_text())
+      print(genre.a.get_text())
+  return genre_list
   
+
+url = "http://movie.naver.com/movie/point/af/list.nhn"
+movie_links_list = get_genre_list(url)
+
+
+res = requests.get("http://movie.naver.com/movie/point/af/list.nhn?st=mcode&sword=187322&target=after")
+content = res.text
+soup = BeautifulSoup(content, 'html5lib')
+page_links = soup.select('a[href]')
+page_link_list = []
+for link in page_links:
+  if re.search(r'&target=after', link['href']):
+    target_url='http://movie.naver.com'+str(link['href'])
+    page_link_list.append(target_url)
+print(page_link_list)
